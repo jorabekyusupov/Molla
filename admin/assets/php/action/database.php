9595f3249bdd86f971215f99a539dda1bb878394
jorabekyusupov <jonistar31@gmail.com>
2021-08-29 14:35:16 +0500

    ss

--- admin/assets/php/action/database.php
+++ admin/assets/php/action/database.php
@@ -3,7 +3,7 @@
         public $__connect;
         public function __construct()
         {
-            $this->__connect = new mysqli('aznewsa.mysql.database.azure.com', 'YUSUPOV@aznewsa', '8118787Aa', 'molla');
+            $this->__connect = new mysqli('s29oj5odr85rij2o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'reussjwwoklyujz4', 'j66i01l0m6cb4ybj', 'bj360w17jfok253q');
         }
         function getConnect()
         {
